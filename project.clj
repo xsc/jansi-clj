57e823a29fe6849ab44ea287ea97af29b13c62ee
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-03-16 22:14:21 +0100

    add 'codox' plugin/alias.

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.fusesource.jansi/jansi "1.11"]]
+  :profiles {:doc {:plugins [[codox "0.6.7"]]
+                   :codox {:exclude [jansi-clj.auto]}}}
+  :aliases {"doc" ["with-profile" "+doc" "doc"]}
   :pedantic? :abort)
