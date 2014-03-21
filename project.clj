07efc97c8408bbfe344845923c0e83db516c5f97
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-03-21 09:02:18 +0100

    Back to SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jansi-clj "0.1.0"
+(defproject jansi-clj "0.1.1-SNAPSHOT"
   :description "Clojure Wrapper around Jansi."
   :url "https://github.com/xsc/jansi-clj"
   :license {:name "Eclipse Public License"
