bdc7aa4ada09ba577fc61f9940b361d72d743518
Yannick Scherer <yannick.scherer@futurice.com>
2017-09-04 08:44:15 +0200

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jansi-clj "0.1.1"
+(defproject jansi-clj "0.1.2-SNAPSHOT"
   :description "Clojure Wrapper around Jansi."
   :url "https://github.com/xsc/jansi-clj"
   :license {:name "Eclipse Public License"
