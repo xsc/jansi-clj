1c8219e6d361d675d7db801a521c33bfa764989d
Yannick Scherer <yannick.scherer@futurice.com>
2023-10-26 09:17:40 +0200

    Remove codox dependency completely

--- project.clj
+++ project.clj
@@ -8,9 +8,4 @@
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
                  [org.fusesource.jansi/jansi "2.4.1"]]
-  :profiles {:doc {:plugins [[codox "0.10.8"]]
-                   :codox {:exclude [jansi-clj.auto]
-                           :src-dir-uri "https://github.com/xsc/jansi-clj/blob/master/"
-                           :src-linenum-anchor-prefix "L"}}}
-  :aliases {"doc" ["with-profile" "+doc" "doc"]}
   :pedantic? :abort)
