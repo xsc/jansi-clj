1caf12202980d1beff8c3eb82e191f98626436bd
Yannick Scherer <yannick@xsc.dev>
2023-12-05 15:15:21 +0100

    Version 1.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jansi-clj "1.0.3-SNAPSHOT"
+(defproject jansi-clj "1.0.3"
   :description "Clojure Wrapper around Jansi."
   :url "https://github.com/xsc/jansi-clj"
   :license {:name "MIT"
